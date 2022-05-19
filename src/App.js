875cc24dc1c8e37eccb8abb407a528d0f416639a
JoseGonzalez098 <gon20335@uvg.edu.gt>
2022-05-19 16:32:03 -0600

    .

--- src/App.js
+++ src/App.js
@@ -7,21 +7,12 @@ class App extends Component {
 
   fetchInven(){
     let inventario = getData();
+    let temp = inventario
     console.log("fetch")
     inventario.map(object => (
-      <div key={object.key}>
-        <div>{object.ID}</div>
-        <div>{object.ID_categoria}</div>
-        <div>{object.ID_marca}</div>
-        <div>{object.calificacion}</div>
-        <div>{object.cant_ventas}</div>
-        <div>{object.cantidad}</div>
-        <div>{object.descripcion}</div>
-        <div>{object.imagen}</div>
-        <div>{object.nombre}</div>
-        <div>{object.precio}</div>
-      </div>
+      console.log(object.ID)
     ))
+    return(temp)
   }
 
   render(){
@@ -36,10 +27,9 @@ class App extends Component {
     return (
       <div className="App">
         <p>HOLA BUENAS TARDES</p>
-        <button onClick={Firedb}>INFO</button>
           <div className='mainContent'>
             {
-              <p>{this.fetchInven()}</p>
+              <div>{this.fetchInven()}</div>
             }
           </div>
       </div>
