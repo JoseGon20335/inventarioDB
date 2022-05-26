5528a2ac34368aa2089282d54f0d1feefc0296b1
DiegoP0805 <60204933+DiegoP0805@users.noreply.github.com>
2022-05-26 15:54:58 -0600

    Correcciones de apariencia

--- src/App.js
+++ src/App.js
@@ -8,29 +8,29 @@ import './App.css';
 
 class App extends Component {
 
-  async hola(){
-
-    console.log("entro a hola")
-
-    let inventario = await getData();
-    let a = inventario[0];
-    console.log("prueba: ", inventario[0])
-    console.log("prueba 2: ", a.nombre)
-
-    console.log("inventario",inventario)
-
-    console.log("inventario2",inventario.length)
-    
-
-    for(var i = 0; i < inventario.length; i++)
-    {
-      console.log(" - funciona: ", inventario[i].nombre)
-    }
-
-    return inventario
-    
-  }
-  componentDidMount() {this.hola()}    
+  //async hola(){
+//
+  //  console.log("entro a hola")
+//
+  //  let inventario = await getData();
+  //  let a = inventario[0];
+  //  console.log("prueba: ", inventario[0])
+  //  console.log("prueba 2: ", a.nombre)
+//
+  //  console.log("inventario",inventario)
+//
+  //  console.log("inventario2",inventario.length)
+  //  
+//
+  //  for(var i = 0; i < inventario.length; i++)
+  //  {
+  //    console.log(" - funciona: ", inventario[i].nombre)
+  //  }
+//
+  //  return inventario
+  //  
+  //}
+  //componentDidMount() {this.hola()}    
   
   render(){
     return (
